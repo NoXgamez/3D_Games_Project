855fd76a832efec28ec737dd80c855970e9f0166
Peter Skrivanek <123364568+S00236650@users.noreply.github.com>
2024-05-11 17:19:38 +0100

    made stats update on menu buttons

--- Assets/Peter/Scripts/Stats.cs
+++ Assets/Peter/Scripts/Stats.cs
@@ -2,56 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Stats : MonoBehaviour
+public class Stats
 {
-    public float exp = 0;
-    public float level = 1;
-    public float expNeeded = 10;
+    public float exp;
+    public float level;
+    public float expNeeded;
 
-    public float upgradePoints = 0;
-    public float levelHealth = 1;
-    public float levelEnergy = 1;
-    public float levelEnergyRegen = 1;
+    public float upgradePoints;
+    public float levelHealth;
+    public float levelEnergy;
+    public float levelEnergyRegen;
 
-    public float maxHealth = 5;
-    public float maxEnergy = 10;
-    public float energyRegen = 1;
+    public float maxHealth;
+    public float maxEnergy;
+    public float energyRegen;
 
+    // Move to player once stats are implemented
     public void LevelUp()
     {
-        level++;
-        upgradePoints++;
-        exp -= expNeeded;
-        expNeeded += Random.Range(10f, 15f);
-    }
-
-    public void HealthUp()
-    {
-        if (upgradePoints >= 1 && levelHealth < 10)
+        if (exp >= expNeeded)
         {
-            maxHealth += 5;
-            levelHealth++;
-            upgradePoints--;
-        }
-    }
-
-    public void EnergyUp()
-    {
-        if (upgradePoints >= 1 && levelEnergy < 10)
-        {
-            maxEnergy += 3;
-            levelEnergy++;
-            upgradePoints--;
-        }
-    }
-
-    public void EnergyRegenUp()
-    {
-        if (upgradePoints >= 1 && levelEnergyRegen < 10)
-        {
-            energyRegen++;
-            levelEnergyRegen++;
-            upgradePoints--;
+            level++;
+            upgradePoints++;
+            exp -= expNeeded;
+            expNeeded += Random.Range(10f, 15f);
         }
     }
 }
